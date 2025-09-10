e258f9b2dde0e9ae98f9e31a5265de0f5d0aced2
ysacaca <ysacaca@mc4.com.bo>
2025-09-10 00:07:39 -0400

    update

--- models/DrawPoint.cs
+++ models/DrawPoint.cs
@@ -14,5 +14,6 @@ namespace models
         public double Longitude { get; set; }
         public string Type { get; set; }
         public string Image { get; set; }
+        //public string Color { get; set; }
     }
 }
