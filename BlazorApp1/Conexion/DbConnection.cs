4e0915427c73d54f2b19ac449bb64e0ad1e1f8f3
kennyesid <kennyesid@gmail.com>
2025-09-21 22:22:22 -0400

    update

---
+++ BlazorApp1/Conexion/DbConnection.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BlazorApp1.Conexion
+{
+    public class DbConnection
+    {
+        private readonly string _connectionString;
+
+        public DbConnection(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        // Método para ejecutar un procedimiento almacenado
+        public async Task<DataTable> ExecuteStoredProcedureAsync(string spName, SqlParameter[] parameters = null)
+        {
+            var dataTable = new DataTable();
+            using (var connection = GetConnection())
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand(spName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        // Método para ejecutar una consulta SQL
+        public async Task<DataTable> ExecuteQueryAsync(string query, SqlParameter[] parameters = null)
+        {
+            var dataTable = new DataTable();
+            using (var connection = GetConnection())
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+    }
+}
